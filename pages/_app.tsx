e727db5dda4395c9dcc31f5c0794109dd6bfb996
matheusnsilva7 <silva.matheus6081@gmail.com>
2023-01-09 16:37:55 -0300

    navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
-import NavBar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-        <NavBar />
+        <Navbar />
         <Component {...pageProps}> </Component>
     </>
   );
