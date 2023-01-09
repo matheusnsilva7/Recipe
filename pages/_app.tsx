5f0644cd4a43ed27dbf09e48769b7058fb9c67dd
Matheus Nascimento <94863819+matheusnsilva7@users.noreply.github.com>
2023-01-09 17:09:23 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
